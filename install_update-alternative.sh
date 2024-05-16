0db9bc18dcb370f5884e54268ea9f0bcf50a46df
Jongyul Kim <yulistic@gmail.com>
2024-05-16 21:59:24 +0000

    Add setup and update alternative script.

--- install_update-alternative.sh
+++ install_update-alternative.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 usage(){
-    echo "$(basename $0) <version>"
+    echo "$(basename $0) [-i <version>]"
 }
 
-while getopts 'h' c
+while getopts 'i:h' c
 do
     case $c in
-	h) usage
-	    exit
-	    ;;
+	i)
+		sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${OPTARG} 10
+		sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-${OPTARG} 10
+		exit
+		;;
+
+	h)
+		usage
+		exit
+		;;
     esac
 done
 
-if [ -z $1 ]; then
-    usage
-    exit
-fi
-
-
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$1 10
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$1 10
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -s /home/yulistic/git_repo/myscripts/install_kernel.sh ~/.local/bin/jkbuild
+ln -s /home/yulistic/git_repo/myscripts/install_update-alternative.sh ~/.local/bin/jch-gcc
